c45561a30a427471c3dd9f0457cd38f19fadd495
KelseyDang2002 <kelseydang813@gmail.com>
2023-09-08 21:24:02 -0700

    coffeerun update

--- coffeerun/scripts/truck.js
+++ coffeerun/scripts/truck.js
@@ -24,7 +24,30 @@
         customerIdArray.forEach(function (id) {
             console.log(this.db.get(id));
         }.bind(this));
-    };       
+    };
+
+    Truck.runTests = function() {
+        var App = window.App;
+        var Truck = App.Truck;
+        var DataStore = App.Datastore;
+        var enterprise = new Truck('ncc1705', new DataStore());
+
+        console.log('---------- running Truck tests ----------')
+        enterprise.createOrder({ emailAddress: 'ryker@starfleet.com', order: 'caramel macciato' });
+        enterprise.createOrder({ emailAddress: 'laforge@starfleet.com', order: 'boba' });
+        enterprise.createOrder({ emailAddress: 'data@starfleet.com', order: 'water' });
+        enterprise.createOrder({ emailAddress: 'picard@starfleet.com', order: 'earl grey hot' });
+        enterprise.createOrder({ emailAddress: 'crusher@starfleet.com', order: 'coca-cola' });
+        enterprise.createOrder({ emailAddress: 'worf@starfleet.com', order: 'triple espresso' });
+        enterprise.printOrders();
+
+        enterprise.deliverOrder('laforge@starfleet.com');
+        enterprise.deliverOrder('crusher@starfleet.com');
+        enterprise.deliverOrder('worf@starfleet.com');
+        enterprise.printOrders();
+
+        console.log('---------- finished Truck tests ----------')
+    };
 
     App.Truck = Truck;
     window.App = App;
