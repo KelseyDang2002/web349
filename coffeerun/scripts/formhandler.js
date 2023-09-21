3d92e961e62c12a0200ff2e3fa351ac7731879a9
KelseyDang2002 <kelseydang813@gmail.com>
2023-09-20 19:45:21 -0700

    ch11 done

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -31,6 +31,20 @@
         });
     };
 
+    FormHandler.prototype.addInputHandler = function (fn) {
+        console.log('Setting input handler for form');
+        this.$formElement.on('input', '[name="emailAddress"]', function (event) {
+            var emailAddress = event.target.value;
+            var message = '';
+            if (fn(emailAddress)) {
+                event.target.setCustomValidity('');
+            } else {
+                message = emailAddress + ' is not an authorized email address!'
+                event.target.setCustomValidity(message);
+            }
+        });           
+    };
+
     FormHandler.runTests = function () {
         var fh = new App.FormHandler('[data-coffee-order="form"]');
         fh.addSubmitHandler();
