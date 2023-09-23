4091d40dd97e6fca29a42f51f525ad7fce64f4a0
KelseyDang2002 <kelseydang813@gmail.com>
2023-09-22 21:17:24 -0700

    React Ch2

---
+++ react-book/ottergramreact/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header () {
+    return (
+        <header className='header-component'>
+            <h1>Ottergram</h1>
+            <h2>instagram for otters i guess</h2>
+        </header>
+    )
+}
+
+export default Header;
